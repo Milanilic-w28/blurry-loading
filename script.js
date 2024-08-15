dda426c200c73f6f4263f13048c16c6824d5a571
Milan Ilic <119868926+Milanilic-w28@users.noreply.github.com>
2024-08-15 21:16:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,6 @@ loadText.style.opacity = scale(load, 0, 100, 1, 0)
 bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-
-
-// stack overflow function that count number to decrement opacity
-// in load text line 16
-
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 }
